f5824797940593318d89ea0a727eb2c4e98f32df
EmilieBrsx <e.brusseaux@gmail.com>
2016-04-01 09:29:02 +0200

    Added files via upload

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour
+{
+    private bool IsSwitch = false;
+    public Camera mainCamera;
+    public Camera fpsCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        mainCamera.enabled = true;
+        fpsCamera.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown (KeyCode.V))   
+        {
+            Debug.Log("changement camera");
+            if (fpsCamera.enabled == false)
+            {
+                fpsCamera.enabled = true;
+            }
+
+            else
+            {
+                fpsCamera.enabled = false;
+            }
+            
+        }
+    }
+}
