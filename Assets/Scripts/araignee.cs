dfd2f369acc57483f9c830b1ac6c81bf6a7156db
maudB <maud.boue@gmail.com>
2016-04-03 20:04:33 +0200

    code de passage de forme floue à araignée (avec jauge moche temporaire)

---
+++ Assets/Scripts/araignee.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class araignee : MonoBehaviour {
+
+	private Animator anim;
+
+	// pour passage de forme floue à araignee
+	public bool joueurEstDansZone;
+	private float jauge;
+	private float tempsAppear = 100;
+	private float coefMonteJauge = 0.8f;
+	private Transform jaugeSprite;
+
+	//pour confrontation
+	private bool estApparu = false;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		jaugeSprite = transform.FindChild ("Jauge");
+		jaugeSprite.localScale = new Vector3 (0,jaugeSprite.localScale.y,jaugeSprite.localScale.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// Quand forme floue
+		if (!estApparu) {
+			if (joueurEstDansZone) {
+				jauge += coefMonteJauge;
+				if (jauge >= tempsAppear) transformationEnAraignee();
+				jaugeSprite.localScale = new Vector3 (jauge*6,jaugeSprite.localScale.y,jaugeSprite.localScale.z);
+			}
+
+			if (!joueurEstDansZone && jauge > 0) {
+				jauge -= coefMonteJauge / 2;
+				jaugeSprite.localScale = new Vector3 (jauge*6,jaugeSprite.localScale.y,jaugeSprite.localScale.z);
+			}
+		}
+
+
+
+	}
+
+	private void transformationEnAraignee(){
+		Debug.Log ("Apparu");
+		estApparu = true;
+		anim.SetBool ("apparu", true);
+		transform.GetComponentInChildren<zoneAraigneeAppear> ().gameObject.SetActive (false);
+		jaugeSprite.gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/zoneAraigneeAppear.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zoneAraigneeAppear : MonoBehaviour {
+
+	private araignee parent;
+
+	// Use this for initialization
+	void Start () {
+		parent = transform.parent.GetComponent<araignee>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.tag == ("Player")) {
+			parent.joueurEstDansZone = true;
+		}
+	}
+
+	void OnTriggerExit(Collider coll){
+		if (coll.gameObject.tag == ("Player")) {	
+			parent.joueurEstDansZone = false;
+		}
+	}
+}
