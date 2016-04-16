228ac1dc253cd25fee16639fcdc27906a489590e
maudB <maud.boue@gmail.com>
2016-04-16 13:18:04 +0200

    écran de debut et de fin

---
+++ Assets/Scripts/startManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startManager : MonoBehaviour {
+
+	public bool startScreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Space)){
+			if (startScreen){
+			Application.LoadLevel("testAraignee");
+			}
+			else {
+			Application.LoadLevel("startScreen");
+			}
+		}
+
+	}
+}
