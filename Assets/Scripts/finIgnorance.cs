6ade3b60c622a9986400d021bc2a3d4d642307b4
maudB <maud.boue@gmail.com>
2016-04-18 18:48:37 +0200

    fin ignorance version simple

--- Assets/Scripts/finIgnorance.cs
+++ Assets/Scripts/finIgnorance.cs
@@ -6,34 +6,43 @@ public class finIgnorance : MonoBehaviour {
 
 	public bool finDuChemin;
 	public Image blanc;
+	private Camera mainCamera;
+	private float alphaBlanc;
+	private Transform joueur;
 
 	// Use this for initialization
 	void Start () {
+		mainCamera = GameObject.FindObjectOfType<Dezoom>().gameObject.GetComponent<Camera>();
+		joueur = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnTriggerEnter(Collider coll){
 		if (coll.gameObject.tag == "Player") {
 			if (finDuChemin) Application.LoadLevel("EndScreenIgnorance");
-			else blanc.gameObject.SetActive(true);
+			else {GameObject.FindObjectOfType<Canvas>().worldCamera=mainCamera;
+				blanc.gameObject.SetActive(true);}
 		}
 	}
 
 	void OnTriggerExit(Collider coll){
 		if (coll.gameObject.tag == "Player") {
-			if (finDuChemin)
-				blanc.gameObject.SetActive (true);
+			//if (!finDuChemin)
+				//alphaBlanc = 0;
+				//blanc.gameObject.SetActive (false);
 		}
 	}
 
 	void OnTriggerStay (Collider coll){
 		if (!finDuChemin && coll.gameObject.tag == "Player") {
 			if (blanc) {
-				blanc.color = new Color (1,1,1,1);}
+				alphaBlanc = (transform.position.x - joueur.position.x)/12;
+				Debug.Log (alphaBlanc);
+				blanc.color = new Color (1,1,1,1-alphaBlanc);}
 			}
 	}
 }
