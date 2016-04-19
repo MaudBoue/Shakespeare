6c263071d9be9080365e0ab09b44868ef53f0917
maudB <maud.boue@gmail.com>
2016-04-19 14:00:54 +0200

    code fin conaissances (lance une anim puis écrant de fin) + personnage tourne mieux

---
+++ Assets/Scripts/finConnaissance.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finConnaissance : MonoBehaviour {
+
+	private Animator anim;
+	public bool animFinie;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		anim.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (animFinie) {
+			Application.LoadLevel("EndScreenConnaissances");
+		}
+	}
+
+	void OnTriggerEnter (Collider coll){
+		if (coll.gameObject.tag == ("Player")){
+			anim.enabled = true;
+			anim.Play("fin");
+		}
+	}
+}
--- Assets/Scripts/move2.cs
+++ Assets/Scripts/move2.cs
@@ -59,7 +59,6 @@ public class move2 : MonoBehaviour {
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) ) {
 			rot = Quaternion.FromToRotation (new Vector3 (0, 0, 1), new Vector3 (rigid.velocity.x, 1, rigid.velocity.z)).eulerAngles.y;
 			Perso.rotation = Quaternion.Euler (0, rot, 0);
-			Debug.Log (rot);
 		}
 	}
 	
